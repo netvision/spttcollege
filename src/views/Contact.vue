4862617cfc796ecf8d4c1e66d6f047a118dd78f4
netvision <rakesh@jangid.co.in>
2022-04-29 10:34:35 +0530

    first update

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,7 +4,7 @@
     <p class="text-xl w-10/12 text-left mx-16 pt-16">Principal</p>
     <p class="text-xl w-10/12 text-left mx-16 pt-5 pb-2 font-bold text-blue-900">Sardar Patel Sikshak Prakshishan Mahavidyalaya</p>
     <p class="text-xl w-10/12 text-left mx-16 pb-2">Katrathal, Sikar, Rajasthan</p>
-    <p class="text-xl w-10/12 text-left mx-16 pb-24"></p>
-    <p class="text-xl w-10/12 text-left mx-16 pb-24"></p>
+    <p class="text-xl w-10/12 text-left mx-16 pb-24">Phone: 9928142474</p>
+    <p class="text-xl w-10/12 text-left mx-16 pb-24">Email: <a href="mailto:sp.ttcollege@gmail.com">sp.ttcollege@gmail.com</a></p>
   </div>
 </template>
