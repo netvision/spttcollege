a6daba488cd5bcf8bdca52e96ce4f955e2c2198b
Rakesh Jangid <rakesh@jangid.co.in>
2024-04-09 12:39:36 +0530

    update

--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -1,14 +1,10 @@
 <script setup>
 import principal from '../assets/principal.jpg'
-import staff from '../assets/teaching.jpg'
 </script>
 <template>
     <div id="teaching">
     <h2 class="text-3xl mx-14 pt-16 pb-0 font-serif border-b-2 border-orange-500 w-10/12">Principal</h2>
     <img :src="principal" class="rounded-md p-5 border-2 m-16 w-10/12" />
   </div>
-  <div id="nonteaching">
-    <h2 class="text-3xl mx-14 pt-16 pb-0 font-serif border-b-2 border-orange-500 w-10/12">Teaching Staff</h2>
-    <img :src="staff" class="rounded-md p-5 border-2 m-16 w-10/12" />
-  </div>
+  
 </template>
