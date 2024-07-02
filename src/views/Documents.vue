fb61f1a2a6b369c5e2957953d075f0abe9142535
Rakesh Jangid <rakesh@jangid.co.in>
2024-07-02 18:46:53 +0530

    updates

--- src/views/Documents.vue
+++ src/views/Documents.vue
@@ -4,11 +4,13 @@
 
 <p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="ncte" target="_blank"><img :src="pdf" class="w-14" /><span>NCTE Recognition Letter</span></a></p>
 <p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="extention" target="_blank"><img :src="pdf" class="w-14" /><span>DCE Extention Letter</span></a></p>
+<p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="noc" target="_blank"><img :src="pdf" class="w-14" /><span>University Affiliation</span></a></p>
 </div>
 </template>
 <script setup>
 import ncte from '../assets/ncte.jpg'
 import extention from '../assets/dce.pdf'
 import pdf from '../assets/pdf17.svg'
+import noc from '../assets/uni-noc.pdf'
 
 </script>
